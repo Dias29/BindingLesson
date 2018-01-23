f5ff374010065f6a595e8becd71049bc0c78a70a
УринбасаровД <@A-305-10>
2018-01-23 17:58:14 +0600

    first

---
+++ BindingLessons/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingLessons
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Ticket> tickets;
+        public MainWindow()
+        {
+            InitializeComponent();
+            tickets = new ObservableCollection<Ticket>
+            {
+                new Ticket
+                {
+                    Id = 1,
+                    TrainNumber = "1A",
+                    Price = 5000,
+                    From = "Astana",
+                    To = "Almaty",
+                    DepartureDate = DateTime.Now,
+                    ArriveDate = DateTime.Now.AddDays(2)
+                },
+
+                new Ticket
+                {
+                    Id = 2,
+                    TrainNumber = "1B",
+                    Price = 20000,
+                    From = "Astana",
+                    To = "Moscow",
+                    DepartureDate = DateTime.Now,
+                    ArriveDate = DateTime.Now.AddDays(7)
+                }
+            };
+
+            dataConteiner.ItemsSource = tickets;
+        }
+
+        private void DataConteinerAddingNewItem(object sender, AddingNewItemEventArgs e)
+        {
+            tickets[1].Id = 100; 
+        }
+    }
+}
---
+++ BindingLessons/Ticket.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingLessons
+{
+    public class Ticket: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int id;
+        //и остальные поля по свойствам
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+                //тоже самое для остальных свойств
+            }
+        }
+        //public int Id { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string TrainNumber { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArriveDate { get; set; }
+        public double Price { get; set; }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName="")
+        {
+            if (PropertyChanged!= null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
